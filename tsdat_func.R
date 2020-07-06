9313d2e9112ea35a966a587aff78f9d756430b3d
ealemar8 <36899154+ealemar8@users.noreply.github.com>
2020-07-06 19:27:47 -0400

    from year number and month number to character dates
    previous inputs were year and month numbers, inputs for function now are dates in "%b %Y" format

--- tsdat_func.R
+++ tsdat_func.R
@@ -1,7 +1,18 @@
 
-#function to return year month xts variable (TRUE = year months, default is false)
-xts_yearmon = function(data, var_name, start_year, 
-                       start_month, end_year, end_month, time, yearmon=F, yearq=F){
+# function to return year month xts variable (TRUE = year months, default is false)
+# dates must be provided in 'Jul-2020' format
+
+xts_yearmon = function(data, var_name, start_date, end_date, time, yearmon=F, yearq=F){
+  start_year = as.numeric(substr(start_date,
+                              nchar(start_date)-4, nchar(start_date)))
+  st_month = as.character(substr(start_date, 1, 3))
+  start_month = as.numeric(ifelse(match(st_month, month.abb)<10, 
+                               paste0('0',match(st_month, month.abb)), match(st_month, month.abb)))
+  end_year = as.numeric(substr(end_date,
+                              nchar(end_date)-4, nchar(end_date)))
+  nd_month = as.character(substr(end_date, 1, 3))
+  end_month = as.numeric(ifelse(match(nd_month, month.abb)<10, 
+                               paste0('0',match(nd_month, month.abb)), match(nd_month, month.abb)))
   elapsed_months <- function(start_year, start_month, end_year, end_month) {
     start_date = paste0(start_year,'-',start_month,'-','01')
     end_date = paste0(end_year,'-',end_month,'-','01')
@@ -35,15 +46,15 @@ xts_yearmon = function(data, var_name, start_year,
       freq = 4
       if(yearq==T){
         data = xts(data[(nrow(data)-elapsed_quarters):nrow(data), var_name], 
-                   order.by = as.yearqtr(index(ts(data[(nrow(data)-elapsed_quarters):nrow(data), var_name], 
-                                                  start = c(start_year,start_month), end = c(end_year, end_month),
-                                                  frequency = freq))), frequency = freq)
+                   order.by = index(ts(data[(nrow(data)-elapsed_quarters):nrow(data), var_name], 
+                                                  start = c(start_year,start_month, 01), end = c(end_year, end_month, 01),
+                                                  frequency = freq)), frequency = freq)
         return(data)
       } else {
         datat = xts(data[(nrow(data)-elapsed_quarters):nrow(data), var_name], 
                     order.by = seq(as.Date(paste0(start_year,'-',start_month,'-','01')),
                                    length = elapsed_quarters+1, by = "quarters"), frequency = freq)
         return(datat)
-      }
+              }
     } 
 }
