8bd69ebe057bd0b7485580d31243e2ece90f86ab
ealemar8 <36899154+ealemar8@users.noreply.github.com>
2020-07-06 18:44:35 -0400

    added function to plot fitted vs actual values from xts data and model

--- tsplots.R
+++ tsplots.R
@@ -59,3 +59,33 @@ acfs = function(data, dif='levels', lagmax){
     return(c(pa,pp))
   }
 }
+
+# fitted vs actual plot
+fitted_actual = function(data, mod, start_date, 
+                         end_date, cap = 'Fitted vs Actual'){
+  st_year = as.numeric(substr(start_date,
+                              nchar(start_date)-4, nchar(start_date)))
+  st_month = as.character(substr(start_date, 1, 3))
+  st_month = as.numeric(ifelse(match(st_month, month.abb)<10, 
+                               paste0('0',match(st_month, month.abb)), match(st_month, month.abb)))
+  nd_year = as.numeric(substr(end_date,
+                              nchar(end_date)-4, nchar(end_date)))
+  nd_month = as.character(substr(end_date, 1, 3))
+  nd_month = as.numeric(ifelse(match(nd_month, month.abb)<10, 
+                               paste0('0',match(nd_month, month.abb)), match(nd_month, month.abb)))
+  fitt=ts(fitted(mod)[which(as.yearmon(index(data))==start_date):nrow(data)],
+          start = c(st_year, st_month), end = c(nd_year, nd_month), frequency = 12)
+  p=plot(fitt, type = 'l', ylab='', xaxt='n', main = cap, lwd=2, pch=10)
+  tsp = attributes(fitt)$tsp
+  dates = seq(as.Date(paste0(st_year,'-','0',st_month,'-','01'),
+                      format='%Y-%m-%d'), by = "month", along = fitt)
+  p
+  axis(1, at = seq(tsp[1], tsp[2], along = fitt),
+       labels = format(dates, "%b %Y"))
+  lines(ts(data[which(as.yearmon(index(data))==start_date):nrow(data), ],
+           start = c(st_year, st_month),
+           end = c(nd_year, nd_month), frequency = 12),
+        col = 'red', lwd=2, pch=10)
+  legend("bottomleft", legend = c("Fitted", "Actual"),
+         col = c('black', 'red'), lty=c(1,1), seg.len=1, lwd=4, pch=10)
+}
