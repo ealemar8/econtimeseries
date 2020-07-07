5f3b0a12cc4621d467bd57ef040582be60ee0979
ealemar8 <36899154+ealemar8@users.noreply.github.com>
2020-07-07 17:03:53 -0400

    Update tsdat_func.R
    start and end dates do not have to be provided now.

--- tsdat_func.R
+++ tsdat_func.R
@@ -1,18 +1,27 @@
 
-# function to return year month xts variable (TRUE = year months, default is false)
-# dates must be provided in 'Jul-2020' format
+# function to generate year month xts variable (TRUE = year months, default is false)
+# for Puerto Rico economic indicators using wide matrix published by Planing Board as input
+# (want to reshape big year month matrix to long dataframe of two columns: 
+# one for month-year and one for the observation)
 
-xts_yearmon = function(data, var_name, start_date, end_date, time, yearmon=F, yearq=F){
+xts_yearmon = function(dataa, var_name, yearmon=F, yearq=F){
+  date = c('Jul', 'Aug', 'Sep', 'Oct', 'Nov',
+           'Dec', 'Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun')
+  dataa$Meses = date
+  data = melt(dataa, id.vars = c('Meses'))
+  names(data) = c('Meses', 'year', var_name)
+  start_date = paste0(data[1, 'Meses'], ' ', data[1, 'year'])
+  end_date = paste0(data[nrow(data), 'Meses'], ' ', data[nrow(data), 'year'])
   start_year = as.numeric(substr(start_date,
-                              nchar(start_date)-4, nchar(start_date)))
+                                 nchar(start_date)-4, nchar(start_date)))
   st_month = as.character(substr(start_date, 1, 3))
   start_month = as.numeric(ifelse(match(st_month, month.abb)<10, 
-                               paste0('0',match(st_month, month.abb)), match(st_month, month.abb)))
+                                  paste0('0',match(st_month, month.abb)), match(st_month, month.abb)))
   end_year = as.numeric(substr(end_date,
-                              nchar(end_date)-4, nchar(end_date)))
+                               nchar(end_date)-4, nchar(end_date)))
   nd_month = as.character(substr(end_date, 1, 3))
   end_month = as.numeric(ifelse(match(nd_month, month.abb)<10, 
-                               paste0('0',match(nd_month, month.abb)), match(nd_month, month.abb)))
+                                paste0('0',match(nd_month, month.abb)), match(nd_month, month.abb)))
   elapsed_months <- function(start_year, start_month, end_year, end_month) {
     start_date = paste0(start_year,'-',start_month,'-','01')
     end_date = paste0(end_year,'-',end_month,'-','01')
@@ -27,7 +36,7 @@ xts_yearmon = function(data, var_name, start_date, end_date, time, yearmon=F, ye
     sd <- as.POSIXlt(start_date)
     4 * (ed$year - sd$year) + (ed$mon - sd$mon)
   }
-  if(time == 'month'){
+  if(grepl(pattern = "Q", x = start_date)==F){
     freq = 12
     elapsedm = abs(elapsed_months(start_year, start_month, end_year, end_month))
     if(yearmon == T){
@@ -42,19 +51,19 @@ xts_yearmon = function(data, var_name, start_date, end_date, time, yearmon=F, ye
                                  length = elapsedm+1,
                                  by = "months"), frequency = freq)
       return(datat)
-    }} else if(time == 'quarter'){
+    }} else {
       freq = 4
       if(yearq==T){
         data = xts(data[(nrow(data)-elapsed_quarters):nrow(data), var_name], 
                    order.by = index(ts(data[(nrow(data)-elapsed_quarters):nrow(data), var_name], 
-                                                  start = c(start_year,start_month, 01), end = c(end_year, end_month, 01),
-                                                  frequency = freq)), frequency = freq)
+                                       start = c(start_year,start_month, 01), end = c(end_year, end_month, 01),
+                                       frequency = freq)), frequency = freq)
         return(data)
       } else {
         datat = xts(data[(nrow(data)-elapsed_quarters):nrow(data), var_name], 
                     order.by = seq(as.Date(paste0(start_year,'-',start_month,'-','01')),
                                    length = elapsed_quarters+1, by = "quarters"), frequency = freq)
         return(datat)
-              }
+      }
     } 
 }
